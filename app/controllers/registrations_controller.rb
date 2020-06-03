3cb264e0e4e9afc8adce771e6183fe9b300221cd
Jason Thompson <jason@jasont.us>
2020-06-03 12:06:44 -0400

    setup associations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,11 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def sign_up_parmas
+  def sign_up_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 
-  def account_update_parmas
+  def account_update_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
   end
 
